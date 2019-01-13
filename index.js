20daf53538a6f03199b34dbce235213c7e94a1f5
Léon Rodenburg <l.rodenburg@instanceof.nl>
2019-01-13 11:40:20 +0100

    Handle subscription confirmations and notifications

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
+require('isomorphic-fetch')
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
-const port = process.env.PORT || 3000
 
-let callCount = 1
+app.use(bodyParser.json())
 
-app.get('/', (req, res) => {
+let postCalls = 0
+let postBody = {}
+
+const handleSubscription = body => {
+  if (!body.SubscribeURL) throw 'No subscribe URL found in JSON body'
+  return fetch(body.SubscribeURL)
+}
+
+app.get('/', async (req, res) => {
   return res.status(200).json({
-    calls: callCount++
+    posts: postCalls,
+    lastPostBody: postBody,
   })
 })
 
+app.post('/', async (req, res) => {
+  console.log(`Received data: ${JSON.stringify(req.body)}`)
+
+  postCalls++
+  postBody = req.body
+
+  if (req.body.Type === 'SubscriptionConfirmation') {
+    const response = await handleSubscription(req.body)
+    return res.status(response.status).end()
+  } else if (req.body.Type === 'Notification') {
+    postBody = req.body
+    return res.status(200).end()
+  } else {
+    return res.status(400).end()
+  }
+})
+
+const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Application listening on port ${port}`))
